720c25aa40a213d73c7f248abaf5756d10e34e1f
Alex Dryden <adryden3@illinois.edu>
2026-03-25 17:08:49 -0400

    fix: revert changes to __init__.py as not needed Co-authored-by: alexdryden <47127862+alexdryden@users.noreply.github.com>

--- arcflow/__init__.py
+++ arcflow/__init__.py
@@ -1,15 +1,1 @@
-"""
-ArcFlow package for syncing ArchivesSpace to ArcLight.
-
-To use ArcFlow, import directly from the main module:
-    from arcflow.main import ArcFlow
-
-Services can be imported independently:
-    from arcflow.services.xml_transform_service import XmlTransformService
-    from arcflow.services.agent_service import AgentService
-
-The top-level import is disabled to avoid eager loading of dependencies.
-"""
-
-# Avoid eager imports to allow services to be imported independently
-# from .main import ArcFlow
+from .main import ArcFlow
